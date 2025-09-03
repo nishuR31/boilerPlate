7a650a5f574a32480f308e9e1a5398b97e53adc0
nishuR31 <nishangithub@gmail.com>
2025-09-03 22:02:39 +0530

    new

---
+++ files/packagejson.js
@@ -0,0 +1,30 @@
+let packagejson = `{
+  "name": "backend",
+  "version": "1.0.0",
+  "description": "A backend for my project",
+  "main": "server.js",
+  "type": "module",
+
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+    "dev": "npx nodemon server.js"
+  },
+  "keywords": [
+    "redis",
+    "cors",
+    "helmet",
+    "cookies",
+    "jsonwebtoken",
+    "vscode",
+    "git",
+    "backend",
+    "scaffolding",
+    "express",
+    "mongoose"
+  ],
+  "author": "nishuR31",
+
+}
+`;
+export default packagejson;
