9daba4ffcdf390d2358106230412043c362614cd
Nishu <nishangithub@gmail.com>
2025-09-07 02:30:05 +0530

    Update logger.middleware.js

--- files/logger.middleware.js
+++ files/logger.middleware.js
@@ -2,7 +2,17 @@ let logger = `
       export default function logger(req, res, next) {
     console.log({
     method: req.method,
-    path: \`\${req.host}\${req.originalUrl}\`,
+    secure:req.secure,
+    urls: {
+        path:
+          req.secure ? "https" : (
+            "http" + "://" + req.host + req.baseUrl + req.url
+          ),
+        hostname: req.hostname,
+        port: req.host.split(":")[req.host.split(":").length - 1],
+        base: req.baseUrl,
+        url: req.url,
+      },
     statusCode: res.statusCode,
     time: new Date().toLocaleString(),
     parameters: req.params,
