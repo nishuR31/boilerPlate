bd41078ec7dcb001339fde6d948564396d52aa36
Nishu <nishangithub@gmail.com>
2025-09-17 01:39:04 +0530

    Update connect.js

--- files/connect.js
+++ files/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.MONGOURI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongo fired up successfully");
   } catch (err) {
     console.error(\`MongoDB extinguished successfully:\${err}\`);
